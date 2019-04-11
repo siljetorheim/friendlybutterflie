3ea0e4de2c47257961574b79d73213134006d809
Marie Presthus <maja5595@gmail.com>
2019-04-11 10:03:33 +0200

    Endret meny til knapper. Lagt inn tekstbokser. Lagt inn alle linker fra wildboy.

--- master.js
+++ master.js
@@ -2,12 +2,16 @@ function getInfo(id) {
   return document.getElementById(id)
 }
 function hide(tall) {
-  let liste = [getInfo("intro"), getInfo("oversikt"), getInfo("details"), getInfo("sammenligning")]
+  let liste = [getInfo("introduksjon"), getInfo("oversikt"), getInfo("detaljer"), getInfo("sammenligning")]
   for (var i = 0; i < liste.length; i++) {
     liste[i].className = "hide"
   }
   if (tall != undefined) {
     liste[tall].className = "active"
   }
-
 }
+
+let beskrivelser = "http://wildboy.uib.no/~tpe056/folk/"
+let befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json"
+let sysselsatte = "http://wildboy.uib.no/~tpe056/folk/100145.json"
+let utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
