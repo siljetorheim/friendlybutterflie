8ec269af3741a14503f199695ce8f74e7efe303b
Marie Presthus <maja5595@gmail.com>
2019-04-11 11:20:16 +0200

    Lagt inn linker i tekstboks. Begynt på oversikt i js.

--- master.js
+++ master.js
@@ -15,3 +15,20 @@ let beskrivelser = "http://wildboy.uib.no/~tpe056/folk/"
 let befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json"
 let sysselsatte = "http://wildboy.uib.no/~tpe056/folk/100145.json"
 let utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
+
+var url = "http://wildboy.uib.no/~tpe056/folk/85432.json";
+
+function lastNed0() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var obj = JSON.parse(xhr.responseText);
+      var obj1 = xhr.responseText
+      console.log(obj);
+      //document.getElementById("demo").innerHTML = obj1;
+    }
+  };
+  xhr.send();
+}
+window.onload = lastNed0;
