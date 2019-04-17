2ba794d34227d6cadf6e997db088f89331a8c1de
Silje Marie Torheim <siljemarietorheim@Elkjps-MacBook-Pro-3.local>
2019-04-17 10:54:00 +0200

    oppdatering

--- master_test.js
+++ master_test.js
@@ -3,17 +3,56 @@ let befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json"
 let sysselsatte = "http://wildboy.uib.no/~tpe056/folk/100145.json"
 let utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
 
-//Bør ha en callback funksjon.
-//Lage klasse/konstruktør
 
-function getNames(callback) {
+function getData(url, callback) {
+  var xhr = new XMLHttpRequest();
+  console.log('url', url);
+  xhr.open("GET", url);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var data = JSON.parse(xhr.responseText);
+
+      callback(data);
+
+    };
+  }
+  xhr.send();
+}
+
+function getNames() {
+  var url = utdanning;
+  console.log(url);
+  getData(url, function(obj){
+    console.log('test', obj["elementer"]);
+    let working_elem = document.getElementById("utdanning");
+
+    //Insert liste
+    working_elem.innerHTML += "<ul>" //+= betyr "adds a value to a variable."
+    for (outpt in obj["elementer"]){
+      working_elem.innerHTML += "<li>" + outpt + "</li>";
+    }
+
+    //Stop liste
+    working_elem.innerHTML += "</ul>"
+
+  })
+}
+
+
+var mapping {
+
+  "0101" : "Halden"
+
+}
+
+/*function getNames(callback) {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", utdanning);
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
       var obj = JSON.parse(xhr.responseText);
-      var obj1 = xhr.responseText
-      console.log(obj["elementer"]);
+      var obj1 = xhr.responseText;
+      console.log('test', obj["elementer"]);
       let working_elem = document.getElementById("utdanning");
 
       //Insert liste
@@ -27,12 +66,19 @@ function getNames(callback) {
     }
   };
   xhr.send();
-}
+}*/
 
 function getIDs(){
   var kommunenumre = [];
-  var kommuner = getNames();
-  for (kommune in kommuner) {
-    kommunenumre.push(kommune.navn.kommunenummer)
-  }
+  var data = getData(utdanning, function (data) {
+    for (kommune in data.elementer) {
+      kommunenumre.push(data.elementer[kommune].kommunenummer);
+
+      if (data.elementer[kommune].kommunenummer == '0101') {
+        console.log(data.elementer[kommune]);
+      }
+
+    }
+    console.log(kommunenumre);
+  });
 }
