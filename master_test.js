c7f166a29f74cfc132ab95694b7cb99507dcaf88
Silje Marie Torheim <siljemarietorheim@Elkjps-MacBook-Pro-3.local>
2019-04-18 19:30:50 +0200

    Ryddet i koden

--- master_test.js
+++ master_test.js
@@ -1,9 +1,11 @@
+
+//The links with the data
 let beskrivelser = "http://wildboy.uib.no/~tpe056/folk/"
 let befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json"
 let sysselsatte = "http://wildboy.uib.no/~tpe056/folk/100145.json"
 let utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
 
-
+//Gets the data from the link
 function getData(url, callback) {
   var xhr = new XMLHttpRequest();
   console.log('url', url);
@@ -19,6 +21,7 @@ function getData(url, callback) {
   xhr.send();
 }
 
+//Gets the names from the link
 function getNames() {
   var url = utdanning;
   console.log(url);
@@ -38,47 +41,28 @@ function getNames() {
   })
 }
 
-
-var mapping {
-
+//testing av mapping
+/*var mapping {
   "0101" : "Halden"
-
-}
-
-/*function getNames(callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", utdanning);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var obj = JSON.parse(xhr.responseText);
-      var obj1 = xhr.responseText;
-      console.log('test', obj["elementer"]);
-      let working_elem = document.getElementById("utdanning");
-
-      //Insert liste
-      working_elem.innerHTML += "<ul>" //+= betyr "adds a value to a variable."
-      for (outpt in obj["elementer"]){
-        working_elem.innerHTML += "<li>" + outpt + "</li>";
-      }
-
-      //Stop liste
-      working_elem.innerHTML += "</ul>"
-    }
-  };
-  xhr.send();
 }*/
 
+
+//Gets the kommunenummer from the links
 function getIDs(){
   var kommunenumre = [];
   var data = getData(utdanning, function (data) {
     for (kommune in data.elementer) {
       kommunenumre.push(data.elementer[kommune].kommunenummer);
-
-      if (data.elementer[kommune].kommunenummer == '0101') {
+//testing
+      /*if (data.elementer[kommune].kommunenummer == '0101') {
         console.log(data.elementer[kommune]);
-      }
-
+      }*/
     }
     console.log(kommunenumre);
   });
+}
+
+//Gets information about the kommuner
+function getInfo(){
+
 }
